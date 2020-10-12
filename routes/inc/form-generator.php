791d0eca93e5209915cad8ac05f39582ac3c9937
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-10-12 23:24:58 +0330

    Added some routes

---
+++ routes/inc/form-generator.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Ready to use
+|--------------------------------------------------------------------------
+*/
+
+Route::resource('fields', 'FieldController');
+Route::resource('forms', 'FormController');
+Route::resource('options', 'OptionController');
+Route::resource('config', 'ConfigController');
+
+
+
+
