58a91db0f44dd461c96d467347cc58ad5aac121d
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-10-12 23:25:07 +0330

    Added translate

---
+++ resources/lang/en/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'select' => 'انتخاب',
+    'multiple-select' => 'انتخاب چندگانه',
+    'string' => 'متن',
+    'array_string' => 'متن چند تایی',
+    'boolean' => 'بولین',
+    'select-image' => 'انتخاب تصویر',
+
+];
