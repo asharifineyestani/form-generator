7bcbe71138ba11ea3e7075bd082910b574e7319b
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-10-14 23:47:27 +0330

    Added some package and modified app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,52 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import vSelect from 'vue-select'
+import VueSelectImage from 'vue-select-image'
+import 'vue-select/dist/vue-select.css';
+import imageCropper from 'vue-image-crop-upload';
+import { ToggleButton } from 'vue-js-toggle-button'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import './custom.scss'
 
+import draggable from 'vuedraggable'
+
+require('vue-select-image/dist/vue-select-image.css')
 require('./bootstrap');
-
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+
+Vue.component('draggable', require('./components/ExampleComponent.vue').default);
+
+
+Vue.component('example-component', draggable);
+Vue.component('field-table', require('./components/FieldTable.vue').default);
+Vue.component('form-view', require('./components/FormView.vue').default);
+Vue.component('form-fields', require('./components/FormFields.vue').default);
+Vue.component('options-table', require('./components/OptionsTable.vue').default);
+Vue.component('v-select', vSelect)
+Vue.component('vue-select-image', VueSelectImage)
+Vue.component('image-crop', imageCropper)
+Vue.component('ToggleButton', ToggleButton)
+
+
+
+
+
+
+
+
+
+
+
 
 const app = new Vue({
-    el: '#app',
+    el: '#main',
+    components: { VueSelectImage }
 });
