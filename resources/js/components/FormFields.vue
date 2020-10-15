9ed1d4e9269b41d89188c8b06c5c878ad85c262f
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-10-15 18:27:24 +0330

    Added deleteField, cloneField and handleActive to FormField

--- resources/js/components/FormFields.vue
+++ resources/js/components/FormFields.vue
@@ -2,11 +2,12 @@
     <b-row class="mt-2">
         <b-col cols="3">
             <div class="p-2 alert alert-secondary">
-                <h3>fields</h3>
+                <h3>افزودن فیلد</h3>
                 <draggable
                     class="list-group"
                     :list="fields"
-                    group="fields">
+
+                    :group="{ name: 'fields', pull: 'clone', put: false }">
                     <div class="list-group-item" v-for="element in fields" :key="element.name">
                         {{element.name}}
                     </div>
@@ -15,14 +16,27 @@
         </b-col>
         <b-col cols="9">
             <div class="p-2 alert alert-primary">
-                <h3>In Progress</h3>
+                <h3>{{form.name}}</h3>
                 <draggable
                     class="list-group"
                     :list="formFields"
                     group="fields">
-                    <div class="list-group-item" v-for="element in formFields" :key="element.name">
-                        {{element.name}}
+
+
+                    <div class="form-group"
+                         v-bind:class="{ active: element.style.active }"
+                         v-for="(element , index) in formFields" :key="index"
+                    >
+                        <label>{{element.name}}</label>
+                        <input type="email" class="form-control" aria-describedby="emailHelp"
+                               :placeholder="element.placeholder" @click="makeActiveField($event , index)">
+                        <small class="form-text text-muted">{{element.description}}</small>
+                        <button class="delete" @click="deleteField(index)">D</button>
+                        <button class="clone" @click="cloneField(element)">C</button>
+
                     </div>
+
+
                 </draggable>
             </div>
         </b-col>
@@ -40,17 +54,34 @@
         },
         data() {
             return {
+                test: null,
                 show: false,
                 fields: [
-                    {name: "text"},
-                    {name: "select"},
+                    {
+                        name: "متن",
+                        type: "text",
+                        icon: "text",
+                        description: "We'll never share your email with anyone else.",
+                        placeholder: "Enter email",
+                        required: true,
+                        style: {
+                            active: false
+                        }
+                    },
+                    {
+                        name: "انتخاب",
+                        type: "select",
+                        icon: "text",
+                    },
                 ],
-                formFields: []
+                formFields: [],
+                form: {
+                    name: "نام فرم"
+                }
 
             }
         },
         created() {
-            this.get();
         },
 
         computed: {
@@ -61,12 +92,94 @@
 
         methods: {
 
-            get() {
-                axios
-                    .get('/api/form-generator/forms/' + this.entityId)
-                    .then(response => (this.fields = response.data.data.fields))
+
+            inActiveFields() {
+                for (let i = 0; i < this.formFields.length; i++)
+                    this.formFields[i].style.active = false;
             },
 
+            makeActiveField(value, index) {
+
+                this.inActiveFields();
+
+                let newField = Object.assign({}, this.formFields, {...this.formFields[index],...{style: {active: true}}});
+                Vue.set(this.formFields, index, newField);
+
+            },
+
+            cloneField(value) {
+
+
+                this.formFields.push(value);
+
+
+                this.makeActiveField(null , this.formFields.length - 1)
+            },
+
+
+            deleteField(index) {
+
+                this.formFields.splice(index, 1);
+
+            }
         }
     }
 </script>
+
+
+<style scoped lang="scss">
+
+    .active {
+        border: 1px yellow solid;
+        background: #f3f397;
+        padding: 15px;
+        margin-bottom: 15px;
+    }
+
+
+    .form-group {
+        position: relative;
+        margin-bottom: 24px;
+    }
+
+
+    button {
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background: black;
+        color: white;
+        position: absolute;
+        right: 15px;
+        bottom: -15px;
+        display: none;
+    }
+
+    .active button {
+        display: block;
+    }
+
+    button.delete {
+        right: 60px;
+        background: red;
+    }
+
+    *[dir="rtl"] {
+        button {
+
+            right: inherit;
+            left: 15px;
+            bottom: -15px;
+
+        }
+
+        button.delete {
+            right: inherit;
+            left: 60px;
+
+        }
+    }
+
+
+</style>
